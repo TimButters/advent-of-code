835c7fcdf4c1caf8e4dcb5038c7c1629a0217a95
Tim Butters <tim.d.butters@gmail.com>
2023-10-30 22:30:22 +0000

    Working Day 11

--- 2015/day-11/solution.hs
+++ 2015/day-11/solution.hs
@@ -1,5 +1,5 @@
 import Data.Char (chr, ord)
-import Data.List (find, isInfixOf)
+import Data.List (find, group, isInfixOf, nub)
 
 numsToChars :: [Int] -> [Char]
 numsToChars = map (chr . (+) 97)
@@ -16,23 +16,38 @@ incrementNums nums = newNums ++ [newInt]
       | otherwise = init nums
 
 checkAscending :: (Eq b, Num b) => [b] -> Bool
-checkAscending nums = [1, 1, 1] `isInfixOf` zipWith (curry (\x -> snd x - fst x)) nums (tail nums)
+checkAscending nums = [1, 1] `isInfixOf` zipWith (curry (\x -> snd x - fst x)) nums (tail nums)
 
 checkForbidden :: [Int] -> Bool
 checkForbidden nums
-    | ord 'i' `elem` nums = False
-    | ord 'l' `elem` nums = False
-    | ord 'o' `elem` nums = False
-    | otherwise = True
+  | (ord 'i' - 97) `elem` nums = False
+  | (ord 'l' - 97) `elem` nums = False
+  | (ord 'o' - 97) `elem` nums = False
+  | otherwise = True
 
--- incrementString :: [Char] -> [Char]
--- incrementString password = numsToChars $ ((charsToNums password) + 1) `mod` 26
+checkDoubles :: [Int] -> Bool
+checkDoubles nums = (length . nub $ map head $ filter (\c -> length c > 1) $ group nums) > 1
+
+checkPassword :: [Int] -> Bool
+checkPassword nums = checkAscending nums && checkForbidden nums && checkDoubles nums
+
+findNextPassword :: [Char] -> [Char]
+findNextPassword password = numsToChars newPassword
+  where
+    nums = charsToNums password
+    newPassword = nextValidPassword nums
+
+nextValidPassword :: [Int] -> [Int]
+nextValidPassword nums = newPassword
+  where
+    nextPassword = incrementNums nums
+    newPassword
+      | checkPassword nextPassword = nextPassword
+      | otherwise = nextValidPassword nextPassword
 
 main :: IO ()
 main = do
   let input = "cqjxjnds"
-  -- print $ numsToChars [0, 1, 2, 3]
-  print $ incrementNums [0, 1, 2, 3, 24]
-  print $ incrementNums [0, 1, 2, 3, 25]
-
--- print $ charsToNums "abcdz"
+  let nextPassword = findNextPassword input
+  print nextPassword
+  print $ findNextPassword nextPassword
