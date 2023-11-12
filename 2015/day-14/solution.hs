13cfba5664f9380a67e5ed9892d90aeb0825841b
Tim Butters <tim.d.butters@gmail.com>
2023-11-12 13:16:51 +0000

    Working components for Part 2
    Now need to map (or likely fold) to iterate the process until the time
    limit is reached. Then the points and distance can be read from the map.

--- 2015/day-14/solution.hs
+++ 2015/day-14/solution.hs
@@ -1,4 +1,8 @@
-import GHC.CmmToAsm.Reg.Linear.State (runR)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.List (maximumBy)
+import Data.Maybe (fromJust)
+
 processLine :: String -> (String, Int, Int, Int)
 processLine line = (name, speed, motion, rest)
   where
@@ -15,9 +19,37 @@ runReindeer (name, speed, motion, rest) time = complete + partial
     complete = time `div` cycle * speed * motion
     partial = min (time `mod` cycle) motion * speed
 
+runReindeerPerSecond :: (Int, Int, Int, Int, Int) -> Int -> (Int, Int, Int, Int, Int)
+runReindeerPerSecond (speed, motion, rest, distance, points) sec = (speed, motion, rest, distance + delta, points)
+  where
+    isMoving = (sec `mod` (motion + rest)) <= motion
+    delta =
+      if isMoving
+        then speed
+        else 0
+
+getWinner :: Map String (Int, Int, Int, Int, Int) -> String
+getWinner m = winner
+  where
+    (winner, _) = maximumBy (\(k1, (_, _, _, d1, _))  (k2, (_, _, _, d2, _)) -> d1 `compare` d2) $ Map.toList m
+
+runReindeerInc :: Map String (Int, Int, Int, Int, Int) -> Int -> Map String (Int, Int, Int, Int, Int)
+runReindeerInc reindeer t = Map.insert winner (speed, motion, rest, distance, points + 1) newReindeer
+  where
+    newReindeer = fmap (`runReindeerPerSecond` t) reindeer
+    winner = getWinner newReindeer
+    (speed, motion, rest, distance, points) = fromJust (Map.lookup winner newReindeer)
+
+runReindeerPart2 :: Map String (Int, Int, Int, Int, Int) -> Map String (Int, Int, Int, Int, Int)
+runReindeerPart2 reindeer = newReindeer
+  where
+    newReindeer = runReindeerInc reindeer 10
+
 main :: IO ()
 main = do
   input <- readFile "input.txt"
   let reindeer = map processLine $ lines input
   let distances = map (`runReindeer` 2503) reindeer
   print $ maximum distances
+  let reindeerMap = Map.fromList $ map (\(n, s, m, r) -> (n, (s,m,r,0, 0))) reindeer
+  print $ runReindeerPart2 reindeerMap
