196dde86e7a729873ca2847b9f0b4a879499461b
Tim Butters <tim.d.butters@gmail.com>
2023-12-09 23:09:03 +0000

    Part 2 complete for day 5

--- 2023/python/day-5/solution.py
+++ 2023/python/day-5/solution.py
@@ -1,6 +1,4 @@
-def load_input(filename: str):
-    with open(filename) as f:
-        return {s: d for m in f.read().split("\n\n") for (s, d) in build_map(m)}
+from math import sqrt
 
 
 def build_map(line: str):
@@ -19,13 +17,35 @@ def build_map(line: str):
     ]
 
 
+def build_map2(line: str):
+    left, right = line.split(":")
+    map_name = left.split()[0]
+    if map_name == "seeds":
+        return [(map_name, list(map(int, right.strip().split())))]
+    return [
+        (
+            map_name,
+            {
+                (int(source), int(source) + int(length)): int(dest) - int(source)
+                for source, dest, length in map(str.split, right.strip().split("\n"))
+            },
+        )
+    ]
+
+
+def load_input(filename: str, builder=build_map):
+    with open(filename) as f:
+        return {s: d for m in f.read().split("\n\n") for (s, d) in builder(m)}
+
+
 def map_value(value: int, mapping) -> int:
     match = [(ks, kd) for (ks, kd) in mapping.keys() if ks <= value < kd]
     return value if not match else value + mapping[match[0]]
 
 
-def part1(mappings):
-    seeds = mappings["seeds"]
+def part1(mappings, seeds=None):
+    if seeds is None:
+        seeds = mappings["seeds"]
     paths = list(mappings.keys())[1:]
     min_path = 10**24
     for seed in seeds:
@@ -37,78 +57,42 @@ def part1(mappings):
     return min_path
 
 
-def part2slow(mappings):
-    seed_ranges = mappings["seeds"]
-    seeds = [s for st, en in zip(seed_ranges[::2], seed_ranges[1::2]) for s in list(range(st, st+en))]
-    paths = list(mappings.keys())[1:]
-    min_path = 10**24
-    for seed in seeds:
-        val = seed
+def part2(mappings, loc_range):
+    paths = list(mappings.keys())[-1:0:-1]
+    seed_set = []
+    for loc in loc_range:
+        val = loc
         for path in paths:
             val = map_value(val, mappings[path])
-        if val < min_path:
-            min_path = val
-    return min_path
+        seed_set.append(val)
+    return seed_set
 
 
-def find_intersection(k1, k2):
-    k1_st, k1_en = k1
-    k2_st, k2_en = k2
-    return (k1_st >= k2_st and k1_st <= k2_en) or (k2_en >= k1_st and k2_en <= k1_en)
+def get_ranges(seeds, seed_ranges):
+    ranges = []
+    for start, length in zip(seed_ranges[::2], seed_ranges[1::2]):
+        for seed in seeds:
+            if start <= seed < (start + length):
+                ranges.append((start, length, seed))
+    return ranges
 
 
-def find_ranges(mappings):
-    paths = list(mappings.keys())[1:]
-    location_ranges = [(1459646077, 1579893769)]
-    # location_ranges = [
-    #     (1459646077, 1579893769),
-    #     (2545941209, 2580528551),
-    #     (224099516, 375808933),
-    #     (181690176, 224099516),
-    #     (2580528551, 2731943364),
-    #     (393220323, 444572577),
-    #     (2731943364, 2812721094),
-    #     (2492137689, 2545941209),
-    #     (2334599269, 2427978815),
-    #     (1579893769, 1729601904),
-    #     (960858811, 1166524785)
-    # ]
-    location_ranges = [(0, 56)]
-    for path in paths[-2::-1]:
-        new_location_ranges = []
-        for loc in location_ranges:
-            new_location_ranges.append([(kst, ken) for (kst, ken), v in mappings[path].items() if find_intersection((kst+v, ken+v), loc)])
-
-        location_ranges = {n for r in new_location_ranges for n in r}
-    return list(location_ranges)
-
-
-def select_seeds(seed_ranges, ranges):
-    #seeds = [s for st, en in zip(seed_ranges[::2], seed_ranges[1::2]) for s in list(range(st, st+en))]
-    seeds = [s for rst, ren in ranges for st, en in zip(seed_ranges[::2], seed_ranges[1::2]) for s in list(range(st, st+en)) if (st >= rst and st <=ren) or (en >= rst and en <= ren)]
-    # Find the seed production set that correspond to the ranges then just generate those??
-    return seeds  #[seed for rst, ren in ranges for seed in seeds if seed >= rst and seed <= ren]
-
-
-def part2(mappings):
-    ranges = find_ranges(mappings)
-    seeds = select_seeds(mappings["seeds"], ranges)
-    paths = list(mappings.keys())[1:]
-    min_path = 10**24
-    for seed in seeds:
-        val = seed
-        for path in paths:
-            val = map_value(val, mappings[path])
-        if val < min_path:
-            min_path = val
-    return min_path
+def get_seeds(seed_ranges):
+    return [s for st, en in zip(seed_ranges[::2], seed_ranges[1::2]) for s in list(range(st, st+en))]
 
 
 if __name__ == "__main__":
-    filename = "test_input.txt"
+    filename = "input.txt"
     maps = load_input(filename)
     print(part1(maps))
-    print(part2(maps))
 
-    # 599623118 too high
-    # 457535844 too high
+    maps2 = load_input(filename, builder=build_map2)
+    st = 0
+    en = 120247692
+    seed_set = part2(maps2, range(st, en, (en - st) // 500000))
+    ranges = get_ranges(seed_set, maps2["seeds"])
+    min_r = min([r[2] for r in ranges])
+    max_r = max([r[2] for r in ranges])
+    print(part1(maps, seeds=list(range(min_r-500, min_r))))
+
+    # 41222968
