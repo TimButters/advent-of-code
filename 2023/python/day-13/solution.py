b6651a8d7d7ae360a5a48292b6c5faafced36c81
Tim Butters <tim.d.butters@gmail.com>
2023-12-18 23:15:47 +0000

    Working on smudged mirror

--- 2023/python/day-13/solution.py
+++ 2023/python/day-13/solution.py
@@ -19,6 +19,18 @@ def load_input(filename: str):
         return ims
 
 
+def parse_image(image):
+    im = []
+    for y, line in enumerate(image.split("\n")):
+        rows = []
+        for x, c in enumerate(list(line)):
+            if c == "#":
+                rows.append((x, y))
+        if rows:
+            im.append(rows)
+    return im
+
+
 def find_reflection(image):
     idx = _find_reflection(image)
     if idx is None:
@@ -27,6 +39,31 @@ def find_reflection(image):
         idx = 100 * idx
     return idx
 
+
+def part2(filename):
+    with open(filename) as f:
+        images = f.read().split("\n\n")
+
+    idxs = []
+    for image in images:
+        orig_idx = find_reflection(parse_image(image))
+        print(len(image))
+        for i in range(len(image)):
+            if image[i] == ".":
+                new_char = "#"
+            elif image[i] == "#":
+                new_char = "."
+            else:
+                continue
+            new_image = image[0:i] + new_char + image[i+1:]
+            idx = find_reflection(parse_image(new_image))
+            if idx is not None and idx != orig_idx:
+                print(i, "done")
+                idxs.append(idx)
+                break
+    return idxs
+
+
 def _find_reflection(image):
     M = len(image)
     for i in range(1, M):
@@ -34,6 +71,7 @@ def _find_reflection(image):
             if check_reflection([[x for x, _ in row] for row in image[i-1::-1]], [[x for x, _ in row] for row in image[i:]]):
                 return i
     return None
+
 
 def check_reflection(im1, im2):
     for r1, r2 in zip(im1, im2):
@@ -48,7 +86,13 @@ def pivot(image):
     return [list(grp) for _, grp in groupby(coords, key=itemgetter(1))]
 
 
+
+
 if __name__ == "__main__":
     filename = "input.txt"
     images = load_input(filename)
     print(sum([find_reflection(image) for image in images]))
+
+    print(sum(part2(filename)))
+
+    #30449
