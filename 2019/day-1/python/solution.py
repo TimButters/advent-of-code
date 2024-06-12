f76a7d9a58e3656faf04899501baa049e1c278e7
Tim Butters <Timothy.Butters@theaccessgroup.com>
2024-06-12 14:45:48 +0100

    2019 Day 1 complete in Python

---
+++ 2019/day-1/python/solution.py
@@ -0,0 +1,28 @@
+def load_data(filename: str) -> list[int]:
+    with open(filename) as f:
+        return [int(line) for line in f.readlines() if line]
+
+
+def required_fuel(mass: int) -> int:
+    return (mass // 3) - 2
+
+
+def part1(modules: list[int]) -> int:
+    return sum([required_fuel(module) for module in modules])
+
+
+def part2(modules: list[int]) -> int:
+    fuel = 0
+    for module in modules:
+        fuel_iteration = required_fuel(module)
+        while fuel_iteration > 0:
+            fuel += fuel_iteration
+            fuel_iteration = required_fuel(fuel_iteration)
+    return fuel
+
+
+if __name__ == "__main__":
+    filename = "../input.txt"
+    data = load_data(filename)
+    print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
