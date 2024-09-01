a8d5cfb3ccc3a442e1e7b1517f089805db55904b
Tim Butters <tim.d.butters@gmail.com>
2024-09-01 22:48:45 +0100

    Asteroid destruction working

--- 2019/day-10/python/solution.py
+++ 2019/day-10/python/solution.py
@@ -1,4 +1,5 @@
 from math import isclose, sqrt, atan2, pi
+from itertools import groupby, cycle
 import sys
 
 
@@ -7,12 +8,18 @@ Grid = list[Point]
 FPoint = tuple[float, float]
 FGrid = list[FPoint]
 
+
 def load_input(filename: str) -> tuple[Grid, int, int]:
     with open(filename) as f:
         lines = f.readlines()
         Y = len(lines)
         X = len(lines[0])
-        grid =  [(x, y) for y, line in enumerate(lines) for x, c in enumerate(line.strip()) if c != "."]
+        grid = [
+            (x, y)
+            for y, line in enumerate(lines)
+            for x, c in enumerate(line.strip())
+            if c != "."
+        ]
         return grid, X, Y
 
 
@@ -30,15 +37,19 @@ def plot_ray(p1: Point, p2: Point) -> list[Point]:
     x2, y2 = p2
 
     if y1 == y2:
-        return [(x, y1) for x in range(min(x1, x2)+1, max(x1, x2))]
+        return [(x, y1) for x in range(min(x1, x2) + 1, max(x1, x2))]
 
     if x1 == x2:
-        return [(x1, y) for y in range(min(y1, y2)+1, max(y1, y2))]
+        return [(x1, y) for y in range(min(y1, y2) + 1, max(y1, y2))]
 
     m, c = _get_params(p1, p2)
     step = -1 if y1 > y2 else 1
 
-    return [(round((y-c)/m), y) for y in range(y1, y2 + step, step) if isclose((y-c)/m, round((y-c)/m))][1:-1]
+    return [
+        (round((y - c) / m), y)
+        for y in range(y1, y2 + step, step)
+        if isclose((y - c) / m, round((y - c) / m))
+    ][1:-1]
 
 
 def count_asteroids(point: Point, grid: Grid) -> int:
@@ -53,8 +64,11 @@ def count_asteroids(point: Point, grid: Grid) -> int:
     return count
 
 
-def part1(grid: Grid, max_x: int, max_y: int) -> int:
-    return max([count_asteroids(asteroid, grid) for asteroid in grid])
+def part1(grid: Grid, max_x: int, max_y: int) -> tuple[Point, int]:
+    return max(
+        [(asteroid, count_asteroids(asteroid, grid)) for asteroid in grid],
+        key=lambda x: x[1],
+    )
 
 
 def cart2pol(point: Point) -> FPoint:
@@ -67,7 +81,9 @@ def cart2pol(point: Point) -> FPoint:
 def rotate_polar_grid(polar_grid) -> FGrid:
     def _rotate(t: float) -> float:
         converted_t = t % (2 * pi)  # Move to 0 - 2PI range
-        converted_t = (converted_t - ((3/4 * 2*pi))) % (2 * pi)  # Make straight up 0 rads
+        converted_t = (converted_t - ((3 / 4 * 2 * pi))) % (
+            2 * pi
+        )  # Make straight up 0 rads
         return converted_t
 
     return [(r, _rotate(t)) for r, t in polar_grid]
@@ -75,7 +91,7 @@ def rotate_polar_grid(polar_grid) -> FGrid:
 
 def grid2polar(grid: Grid, point: Point = (0, 0)) -> FGrid:
     px, py = point
-    shifted_grid = [(x-px, y-py) for x, y in grid]
+    shifted_grid = [(x - px, y - py) for x, y in grid]
     polar_grid = [cart2pol(p) for p in shifted_grid]
     return rotate_polar_grid(polar_grid)
 
@@ -83,5 +99,20 @@ def grid2polar(grid: Grid, point: Point = (0, 0)) -> FGrid:
 if __name__ == "__main__":
     filename = sys.argv[1]
     asteroids, X, Y = load_input(filename)
-    max_count = part1(asteroids, X, Y)
+    station, max_count = part1(asteroids, X, Y)
     print("Part 1:", max_count)
+
+    polar = [(r, theta) for r, theta in grid2polar(asteroids, station) if r != 0]
+    polar.sort(key=lambda x: (x[1], x[0]))
+    targets = [
+        (angle, list(grp)[::-1]) for angle, grp in groupby(polar, key=lambda x: x[1])
+    ]
+    c = cycle(targets)
+
+    destroyed = []
+    while len(destroyed) < 6:
+        theta, coords = next(c)
+        if coords:
+            destroyed.append(coords.pop())
+
+    last_asteroid = destroyed[-1]
