e8a22dd2898a66c745ae6e2bf1da2c9ee5614c0f
Tim Butters <tim.d.butters@gmail.com>
2025-12-07 21:34:41 +0000

    Day 3 complete

--- 2025/day-03/python/solution.py
+++ 2025/day-03/python/solution.py
@@ -15,11 +15,11 @@ def max_location(battery: list[int]) -> tuple[int, int]:
 def max_joltage(battery: list[int]) -> int:
     n = len(battery)
     max_idx, max_tens = max_location(battery)
-    if max_idx < n-1:
-        _, max_unit = max_location(battery[max_idx+1:])
+    if max_idx < n - 1:
+        _, max_unit = max_location(battery[max_idx + 1 :])
     else:
         max_idx, max_tens = max_location(battery[:-1])
-        _, max_unit = max_location(battery[max_idx+1:])
+        _, max_unit = max_location(battery[max_idx + 1 :])
     return int(str(max_tens) + str(max_unit))
 
 
@@ -27,9 +27,33 @@ def part1(batteries: list[list[int]]) -> int:
     return sum([max_joltage(battery) for battery in batteries])
 
 
+def max_digit(battery: list[int], digit: int) -> tuple[int, int]:
+    # print(battery, len(battery), digit)
+    if digit == 1:
+        max_j = max(battery)
+    else:
+        max_j = max(battery[: -(digit - 1)])
+    max_idx = battery.index(max_j)
+    return max_idx, max_j
+
+
+def max_joltage_by_digit(battery: list[int], digits: int = 12) -> int:
+    values = []
+    start_idx = 0
+    for d in range(digits, 0, -1):
+        idx, joltage = max_digit(battery[start_idx:], d)
+        start_idx = start_idx + idx + 1
+        values.append(str(joltage))
+    return int("".join(values))
+
+
+def part2(batteries: list[list[int]]) -> int:
+    return sum([max_joltage_by_digit(battery) for battery in batteries])
+
+
 if __name__ == "__main__":
     filename = sys.argv[1]
     input = load_input(filename)
 
     print("Part 1:", part1(input))
-    
+    print("Part 2:", part2(input))
